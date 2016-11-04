595928f20c58f40027a960533cad83d931351afa
Akkharawat Chayapiwat <aukwat@gmail.com>
2016-11-04 00:19:28 -0400

    Initial Project 2

---
+++ Project02/Project02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project02
+//
+//  Created by AKKHARAWAT CHAYAPIWAT on 11/4/16.
+//  Copyright © 2016 AKKHARAWAT CHAYAPIWAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
