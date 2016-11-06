9be5b1292bbfa1055978b45f68a7cb3c409f26e1
Akkharawat Chayapiwat <aukwat@gmail.com>
2016-11-06 17:56:55 -0500

    Init Project 5

---
+++ Project05/Project05/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project05
+//
+//  Created by AKKHARAWAT CHAYAPIWAT on 11/6/16.
+//  Copyright © 2016 AKKHARAWAT CHAYAPIWAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
